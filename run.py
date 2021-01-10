d936cf39e98bc979bf57d67d977caa3188f98f98
Ondřej Divina <Ondra.Divina@seznam.cz>
2021-01-10 17:28:44 +0100

    Parsing of events - Event - namedtuple added - event info parsing

--- run.py
+++ run.py
@@ -1,27 +1,36 @@
+from collections import namedtuple
 import logging as logger
 import time as time
 from bs4 import BeautifulSoup
 from requests import request, status_codes
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import pickle as pickle
 import icalendar as ical
 
 
 class BaseParser(object):
+    Event = namedtuple("Event", field_names=["time", "name", "url", "place"], defaults=["", "", "", ""])
+
     def __init__(self):
         pass
 
     def process(self):
         pass
 
+    def export_to_cal(self, file_path = "Events.ics"):
+        pass
+
 
 class Biathlon(BaseParser):
-    def __init__(self, season_year = None, event_name = None):
+    def __init__(self, season_year: int = None, event_name: str = None):
         super().__init__()
-        self.website = f"https://www.biathlonworld.com/calendar/season/{season_year}/"
-        self.event_name = event_name
-        self.web_content = None
-        self.webdriver = webdriver
+        self.website: str = f"https://www.biathlonworld.com/calendar/season/{season_year}/"
+        self.event_name: str = event_name
+        self.web_content: BeautifulSoup or None = None
+        self.webdriver: webdriver = webdriver
+        self.parsed_events = []
 
     def get_website_content(self):
         if request("GET", self.website).status_code is not status_codes.codes.ok:
@@ -32,27 +41,37 @@ class Biathlon(BaseParser):
 
         # TODO try/except
         self.setup_webdriver()
-        events = self.webdriver.find_elements_by_css_selector("h3 > a")
+        # events = self.webdriver.find_elements_by_css_selector("h3 > a")
+        # select only non expanded events
+        events = self.webdriver.find_elements_by_xpath("//a[contains(@class,'js-title collapsed')]")
         logger.log(logger.DEBUG, f"Total of {len(events)} headers located on the page.")
         logger.log(logger.INFO, f"Total of {len(events)} events found.")
 
         self.web_content = []
         for event in events:
             event.click()
-            time.sleep(1)
-            page_html = self.webdriver.page_source
-            self.web_content.append(BeautifulSoup(page_html, "lxml"))
-            logger.log(logger.DEBUG, msg=f"Page source of event was successful and added to self.web_content")
+        time.sleep(0.25)
+        page_html = self.webdriver.page_source
+        self.web_content = BeautifulSoup(page_html, "lxml")
+        logger.log(logger.DEBUG, msg=f"Page source of event was successful and added to self.web_content")
         logger.log(logger.INFO, msg=f"Getting content of the web is complete")
 
     def parse(self):
-        pass
+        events = self.web_content.find_all("li", {"class": "dcm-competition"})
+        for event in events:
+            time_str = event.find("span", {"class": "dcm-date"}).text
+            event_name = event.find("span", {"class": "dcm-competition--name"}).text
+            self.parsed_events.append(Biathlon.Event(name=event_name, time=time_str))
 
     def export_to_cal(self, file_path = "Biathlon_events.ics"):
         pass
 
     def process(self):
-        self.get_website_content()
+        if not self.web_content:
+            self.get_website_content()
+
+        self.parse()
+        self.export_to_cal()
 
     def setup_webdriver(self):
         opts = Options()
